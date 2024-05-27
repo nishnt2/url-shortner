3e3c6191e173cefdc0266a627580e316c1d0f9f5
nishnt2 <patilnishant222@gmail.com>
2024-05-27 23:58:34 +0530

    dynamic route for redirection

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const useRoute = require('./routes/url');
+
+const URL = require('./models/url');
+
 const connectToMongoDB = require('./connection');
 const app = express();
 const PORT = 8001;
@@ -7,6 +10,18 @@ const PORT = 8001;
 connectToMongoDB('mongodb://127.0.0.1:27017/url-short');
 app.use(express.json);
 app.use('/url', useRoute);
+app.get('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    { $push: { visitHistory: { timeStamp: Date.now() } } }
+  );
+
+  res.redirect(entry.redirectURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
